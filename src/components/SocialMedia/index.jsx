dd779f6b5a272208b9eda0e1f136493903eaf63d
Damien Alves <alves.d24@laposte.net>
2023-02-07 16:58:28 +0100

    feat(sociamedia): add target twitter in link

--- src/components/SocialMedia/index.jsx
+++ src/components/SocialMedia/index.jsx
@@ -7,12 +7,16 @@ import "./index.css";
 function SocialMedia() {
   return (
     <div className="container-social-media">
-      <svg width={65} height={60}>
-        <image href={twitterMedia} width={55} height={55} />
-      </svg>
-      <svg width={55} height={60}>
-        <image href={openSeaMedia} width={55} height={55} />
-      </svg>
+      <a href={import.meta.env.VITE_TWITTER} target="_blank">
+        <svg width={65} height={60}>
+          <image href={twitterMedia} width={55} height={55} />
+        </svg>
+      </a>
+      <a href="https://www.w3schools.com" target="_blank">
+        <svg width={55} height={60}>
+          <image href={openSeaMedia} width={55} height={55} />
+        </svg>
+      </a>
     </div>
   );
 }
